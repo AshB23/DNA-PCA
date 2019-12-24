d3d7e1d78496aa2922488a0ff03097463f4c4233
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2019-12-24 22:25:02 +0530

    Minor Changes/Comments

--- plotDataPoints.m
+++ plotDataPoints.m
@@ -1,15 +1,7 @@
 function plotDataPoints(X, names)
-%PLOTDATAPOINTS plots data points in X, coloring them so that those with the same
-%index assignments in idx have the same color
-%   PLOTDATAPOINTS(X, idx, K) plots data points in X, coloring them so that those 
-%   with the same index assignments in idx have the same color
 
-% Create palette
-
-
-% Plot the data
-scatter(X(:,1), X(:,2), 45);
-dx = 0.1;
-text(X(:,1)+dx,X(:,2)+dx,names);
+	scatter(X(:,1), X(:,2), 45); %plot first two principle components w/ size of 45
+	dx = 0.5;
+	text(X(:,1)+dx,X(:,2)+dx,names);
 
 end
